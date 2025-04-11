8e9453e33db79fbfc9b36602dfd34e2cd58b9f41
quang-design <anhquang1806@gmail.com>
2025-04-11 17:34:58 +0700

    Add draggable and resizable element using InteractJS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import interact from 'interactjs';
+	import type { Action } from 'svelte/action';
+
+	const position = $state({ x: 0, y: 0 });
+
+	const draggable: Action<HTMLDivElement> = (node) => {
+		const style = window.getComputedStyle(node);
+		const matrix = new DOMMatrixReadOnly(style.transform);
+		position.x = matrix.m41;
+		position.y = matrix.m42;
+
+		$effect(() => {
+			const interaction = interact(node)
+				.draggable({
+					listeners: {
+						start(event) {
+							console.log(event.type, event.target, $state.snapshot(position));
+						},
+						move(event) {
+							position.x += event.dx;
+							position.y += event.dy;
+
+							event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+						},
+						end(event) {
+							console.log(event.type, event.target, $state.snapshot(position));
+						}
+					}
+				})
+				.resizable({
+					edges: { top: true, left: true, right: true, bottom: true },
+					listeners: {
+						move: function (event) {
+							position.x += event.deltaRect.left;
+							position.y += event.deltaRect.top;
+
+							Object.assign(event.target.style, {
+								width: `${event.rect.width}px`,
+								height: `${event.rect.height}px`,
+								transform: `translate(${position.x}px, ${position.y}px)`
+							});
+						}
+					}
+				});
+
+			return () => {
+				interaction.unset();
+			};
+		});
+	};
+	$inspect(position);
+</script>
+
+<div
+	class="m-4 h-24 min-h-20 w-36 min-w-28 touch-none rounded-xl bg-sky-400 p-4 text-white select-none"
+	use:draggable
+>
+	Draggable Element
+</div>
